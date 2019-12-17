1f028957a909668480bae1f3a55f5404c6976f35
Afshar20922019 <afshar2092@yahoo.com>
2019-12-17 02:06:26 +0100

    addHoroscope.php

---
+++ server/addHoroscope.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+$month = substr($_POST['birthDate'],2,2);
+$day = substr($_POST['birthDate'],4,2);
+
+include "calculateHoroscope.php";
+
+if(!isset($_SESSION['horoscope']))
+$_SESSION['horoscope'] = $horoscope;
+echo true;
+}
+
+else {
+echo false;
+}
+?>
